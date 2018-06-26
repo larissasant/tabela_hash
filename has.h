1e13c4c07c8ed661ea762ce9e1ed25a228d396e6
larissasant <soulari_@hotmail.com>
2018-06-25 22:01:46 -0400

    Add files via upload

---
+++ has.h
@@ -0,0 +1,22 @@
+#ifndef HAS_H_INCLUDED
+#define HAS_H_INCLUDED
+#include "te.h"
+
+typedef struct hash{
+    Arv **tabela;
+    int size;
+}hash_n;
+
+hash_n* criar_has(int valor);
+
+void insere_has(hash_n* has, int valor);
+void remover_has(hash_n* has, int valor);
+
+void imprimir_has(hash_n* has);
+void imprimir_PreOrdem_has(hash_n* has);
+void imprimir_EmOrdem_has(hash_n* has);
+void imprimir_PosOrdem_has(hash_n* has);
+
+
+#endif // HASH_H_INCLUDED
+
